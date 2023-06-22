aa77e6e939bb11f94413b9641979b97601e713e6
illia flekss <igrishkoo@gmail.com>
2023-06-23 00:39:19 +0300

    Graphic arts + new type of imports

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from random import randint
 
 from graphic_arts.start_game_banner import run_screensaver
+
+
 def attack(char_name, char_class):
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
@@ -9,7 +11,7 @@ def attack(char_name, char_class):
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(5, 10)}')
     if char_class == 'healer':
-        return (f'{char_name} нанёс противнику урон, равный '
+        (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(-3, -1)}')
 
 
@@ -78,14 +80,14 @@ def choice_char_class() -> object:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-    
